38ea79c33077997ab188bd9af7e80fb56040fc6b
Eric Bittleman <eric.bittleman@gmail.com>
2014-03-14 21:18:49 -0400

    small tweak to first test

--- worldone_test.go
+++ worldone_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestAdd(t *testing.T) {
-	expected, actual := 2, Add(1, 1)
+	expected, actual := 3, Add(1, 2)
 
 	if expected != actual {
 		t.Errorf("Expected %d But Got %d", expected, actual)
